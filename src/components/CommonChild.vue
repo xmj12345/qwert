b4861b163f89770e3f503c2d90a005358d35e39f
xmj <xmj@163.com>
2023-09-22 20:56:05 +0800

    校验

--- src/components/CommonChild.vue
+++ src/components/CommonChild.vue
@@ -1,18 +1,10 @@
 <template>
    <h2>这是子组件</h2>
-   {{ title }}
-   <hr>
-   <button @click="title='值改变了'">修改props</button>
-   {{ msg }}
 </template>
 
 <script>
     export default {
-        // 子组件定义props属性接收父组件传递的自定义属性
-        props:['title','msg'],
-        mounted(){
-            console.log(this)
-        }
+       
     }
 </script>
 
