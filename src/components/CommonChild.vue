fa9ac2f1b118767ba5d6a31aca40d97ecf0bec71
xmj <xmj@163.com>
2023-09-22 17:38:03 +0800

    55555

---
+++ src/components/CommonChild.vue
@@ -0,0 +1,20 @@
+<template>
+   <h2>这是子组件</h2>
+   {{ title }}
+   <hr>
+   {{ msg }}
+</template>
+
+<script>
+    export default {
+        // 子组件定义props属性接收父组件传递的自定义属性
+        props:['title','msg'],
+        mounted(){
+            console.log(this)
+        }
+    }
+</script>
+
+<style  lang="scss" scoped>
+
+</style>
--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        <h1>这是子组件</h1>
-    </div>
+        <h1>这是父组件</h1>
+        <CommonChild :title="title" msg="这是静态的值"/>
 </template>
 
 <script>
+import CommonChild from './CommonChild.vue';
     export default {
-        
+        data() {
+            return {
+                title: '这是父组件的数据'
+            }
+        },
+        components:{
+            CommonChild
+        }
     }
 </script>
 
