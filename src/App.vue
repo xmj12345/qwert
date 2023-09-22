bb251c492b8966bb1f00968e27f4d3a8f8bb88fb
xmj <xmj@163.com>
2023-09-22 17:37:39 +0800

    11111

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <template>
-    <h1>这是组件视图内容</h1>
-    <CommonHead />
+  <CommonHead/>
 </template>
+
 <script>
-import CommonHead from "./components/commonHead.vue";
+import CommonHead from './components/CommonHead.vue'
 export default {
-  name: "App",
-  data() {
-    return {
-      msg: "你好呀",
-    };
-  },
   components: {
-    CommonHead,
-  },
-};
+    CommonHead
+  }
+}
 </script>
-<style scoped>
-/* @import url('./assets/style.css'); */
-h1 {
-  color: red;
-}
-:deep h1 {
-  color: blue;
-}
-</style>
