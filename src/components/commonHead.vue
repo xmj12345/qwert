ff6cd2dab2fcde22bd716710693e5bdbdcad8f7b
xmj <xmj@163.com>
2023-09-23 09:53:51 +0800

    子向父完成了

--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,14 +1,27 @@
 <template>
         <h1>这是父组件</h1>
-        <CommonChild :a="10" b="20" c="" e="小红红"/>
+        <CommonChild @biubiu="receiveMsg"/>
+        {{ msg }}
 </template>
 
 <script>
 import CommonChild from './CommonChild.vue';
     export default {
+        data() {
+            return {
+                msg: ''
+            }
+        },
         components:{
             CommonChild
-        }
+        },
+        methods: {
+            receiveMsg(msg) {
+                this.msg = msg
+                console.log('我收到了你的信息')
+            }
+        },
+
     }
 </script>
 
