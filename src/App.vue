21f76b88abf60db56411e798e0a7a84e1f9821aa
xmj <xmj@163.com>
2023-09-23 16:49:54 +0800

    2222

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <CommonHead/>
+  <CommonHead v-if="isShow"/>
+  <button @click="isShow=!isShow">{{ isShow? '隐藏':'显示' }}</button>
 </template>
 
 <script>
 import CommonHead from './components/CommonHead.vue'
 export default {
+  data() {
+    return {
+      isShow: true
+    }
+  },
   components: {
     CommonHead
   }
--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,68 +1,32 @@
 <template>
-  <div class="swiper banner">
-    <div class="swiper-wrapper">
-      <div
-        class="swiper-slide"
-        v-for="bannerImg in banners"
-        :key="bannerImg.id"
-      >
-        <img :src="bannerImg.picUrl" alt="" />
-      </div>
+    <div>
+        <h2>组件</h2>
     </div>
-  </div>
 </template>
 
 <script>
-import "swiper/swiper-bundle.css";
-import Swiper from "swiper";
-import axios from "axios";
-export default {
-  data() {
-    return {
-      swiper: null,
-      banners: [],
-    };
-  },
-  methods: {
-    initSwiper() {
-      let mySwiper = new Swiper(".banner", {
-        autoplay: true, //可选选项，自动滑动
-        loop: true,
-      });
-      this.swiper = mySwiper;
-    },
-    fetchBanners() {
-      axios.get("https://api.it120.cc/conner/banner/list").then((res) => {
-        console.log(res);
-        if (res.data.code === 0) {
-          this.banners = res.data.data;
-          this.$nextTick(() => {
-            this.initSwiper();
-          });
+    export default {
+        data() {
+            return {
+                time: null
+            }
+        },
+        mounted () {
+            let time = setInterval(()=>{
+                console.log('定时器出发了')
+            },1000);
+            this.time = time
+            window.onscroll = function(){
+                console.log('滚动了')
+            }
+        },
+        beforeUnmount(){
+            clearInterval(this.time)
+            window.onscroll = null
         }
-      });
-    },
-  },
-  mounted() {
-    this.fetchBanners();
-  },
-};
+    }
 </script>
 
-<style scoped>
-.banner {
-  width: 600px;
-  height: 400px;
-  border: 1px solid black;
-  margin: 0 auto;
-  .swiper-slide {
-    width: 600px;
-    height: 400px;
-    background-color: skyblue;
-  }
-}
-img {
-  width: 600px;
-  height: 400px;
-}
+<style  scoped>
+
 </style>
