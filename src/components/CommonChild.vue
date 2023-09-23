e653b35f55efe834f99dfd77c4fc89c4f901db3e
xmj <xmj@163.com>
2023-09-23 14:34:58 +0800

    qweqwe

--- src/components/CommonChild.vue
+++ src/components/CommonChild.vue
@@ -1,10 +1,9 @@
 <template>
-   <h2>这是子组件1</h2>
+   <h2>这是子组件</h2>
 </template>
 <script>
     export default {
         data() {
-
             return {
                 msg: '子组件的数据'
             }
@@ -12,6 +11,6 @@
     }
 </script>
 
-<style  lang="scss" scoped>
+<style  scoped>
 
 </style>
--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -14,6 +14,6 @@ import CommonChild from './CommonChild.vue';
     }
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
     
 </style>
