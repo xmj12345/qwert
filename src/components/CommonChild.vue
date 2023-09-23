37b5ce78857ec25ba318e33005d0307344a7680e
xmj <xmj@163.com>
2023-09-23 10:06:18 +0800

    1111

--- src/components/CommonChild.vue
+++ src/components/CommonChild.vue
@@ -1,20 +1,10 @@
 <template>
    <h2>这是子组件</h2>
-   <button @click="noticeMsg">点击通知父组件</button>
 </template>
 
 <script>
     export default {
-        data() {
-            return {
-                msg: '子组件的数据'
-            }
-        },
-     methods: {
-        noticeMsg() {
-            this.$emit('biubiu',this.msg)
-        }
-     },
+     
     }
 </script>
 
--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,26 +1,13 @@
 <template>
         <h1>这是父组件</h1>
-        <CommonChild @biubiu="receiveMsg"/>
-        {{ msg }}
+        <CommonChild/>
+
 </template>
 
 <script>
 import CommonChild from './CommonChild.vue';
     export default {
-        data() {
-            return {
-                msg: ''
-            }
-        },
-        components:{
-            CommonChild
-        },
-        methods: {
-            receiveMsg(msg) {
-                this.msg = msg
-                console.log('我收到了你的信息')
-            }
-        },
+
 
     }
 </script>
