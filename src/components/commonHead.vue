1a080308a46df5117a5263a4d18ca239b6ccdd0b
xmj <xmj@163.com>
2023-09-25 16:12:35 +0800

    mixin1

--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,28 +1,22 @@
 <template>
     <div>
-        <h1>父组件</h1>
-        <hr>
-        <button @click="isShow = !isShow">{{ isShow?'隐藏':'显示' }}</button>
-        <KeepAlive :include="['CommonChild1','CommonChild2']">
-            <component :is="componentName" v-if="isShow"></component>
-        </KeepAlive>
-        
+        <button @click="changeMsg">修改msg</button>
+        {{ msg }}
+        {{ reverseMsg }}
     </div>
 </template>
 
 <script>
-    import CommonChild1 from './CommonChild1.vue';
-    import CommonChild2 from './CommonChild2.vue';
+import mixin1 from '@/mixins/mixin1'
     export default {
         data() {
             return {
-                isShow:true,
-                componentName: 'CommonChild2'
+                msg: '组件自己的数据'
             }
         },
-        components:{
-            CommonChild1,
-            CommonChild2
+        mixins:[mixin1],
+        mounted(){
+            console.log(this)
         }
     }
 </script>
---
+++ src/mixins/mixin1.js
@@ -0,0 +1,17 @@
+export default {
+    data() {
+        return {
+            msg: '混入对象上的数据'
+        }
+    },
+    methods: {
+        changeMsg() {
+            this.msg = '数据改变了'
+        }
+    },
+    computed:{
+        reverseMsg(){
+        return  this.msg.split('').reverse().join('')
+        }
+    }
+}
