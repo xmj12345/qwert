f9e862b078d9fd54abb59bb012b9b1eedae08b00
xmj <xmj@163.com>
2023-09-25 19:49:34 +0800

    作用域插槽

--- src/components/CommonChild.vue
+++ src/components/CommonChild.vue
@@ -1,12 +1,17 @@
 <template>
-    <slot name="header"></slot>
-    <slot></slot>
-    <slot name="footer"></slot>
+    <h1>子组件头部</h1>
+    <slot :a="a" :b="b"></slot>
+    <h1>子组件尾部</h1>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                a:'子组件数据a',
+                b:'子组件数据b'
+            }
+        },
     }
 </script>
 
--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,17 +1,8 @@
 <template>
     <div>
-        <CommonChild>
-            <template #header>
-                <h1>这是传给头部的</h1>
-            </template>
-            <div>
-                <h1>这是传给默认的</h1>
-                <button>这也是传给默认的</button>
-            </div>
-                
-            <template #footer>
-                <mark>这是传给尾部的</mark>
-            </template>
+        <CommonChild v-slot="data">
+          <h3>父组件传入的 {{ data.a }}</h3>
+          <h4>也是父组件传入的 {{ data.b }}</h4>
         </CommonChild>
     </div>
 </template>
