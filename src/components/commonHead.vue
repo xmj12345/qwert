d9889de9be95882a2467f3eafaa6adc911b04923
xmj <xmj@163.com>
2023-09-26 11:19:14 +0800

    121212

--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,53 +1,126 @@
 <template>
-  <button @click="index++">
-    {{ index % 3 }}
-  </button>
-  <!-- out-in 先出后入 -->
-  <Transition name="ani1" mode="out-in">
-    <div class="box1" v-if="index % 3 ===0"></div>
-    <div class="box2" v-else-if="index % 3 ===1"></div>
-    <div class="box3" v-else></div>
-  </Transition>
+  <div class="todo-container">
+    <div class="todo-header">
+      <input type="text" v-model.trim="todoInput" class="todo-input" />
+      <button class="add" @click="addTodo">增加</button>
+    </div>
+    <div class="todo-lists">
+      <div class="todo-item" v-if="todos.length === 0">暂无待办事项</div>
+      <TransitionGroup name="ani">
+        <div
+          :class="[
+            'todo-item',
+            {
+              'todo-done': todo.done,
+              'todo-undone': !todo.done,
+            },
+          ]"
+          v-for="(todo, index) in todos"
+          :key="index"
+        >
+          <span>
+            {{ todo.content }}
+            ---->
+            {{ index }}
+          </span>
+          <div class="actions">
+            <button @click="delTodo(index)">删除待办事项</button>
+            <input type="checkbox" v-model="todo.done" />
+          </div>
+        </div>
+      </TransitionGroup>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      index:0
+      todoInput: "",
+      todos: [
+        {
+          content: "明天记得带伞",
+          done: false,
+        },
+        {
+          content: "内容2",
+          done: true,
+        },
+        {
+          content: "内容3",
+          done: false,
+        },
+      ],
     };
+  },
+  methods: {
+    addTodo() {
+      if (this.todoInput === "") {
+        return false;
+      }
+      this.todos.push({
+        content: this.todoInput,
+        done: false,
+      });
+      this.todoInput = "";
+    },
+    delTodo(index) {
+      if (confirm("确定删除吗")) {
+        this.todos.splice(index, 1);
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
-.box1 {
-  width: 200px;
-  height: 200px;
-  background-color: skyblue;
+.todo-container {
+  max-width: 600px;
+  padding: 30px;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+  margin: 20px auto;
 }
-.box2 {
-  width: 200px;
-  height: 200px;
-  background-color:palegoldenrod;
+.todo-header {
+  height: 80px;
+  display: flex;
+  align-items: center;
 }
-.box3 {
-  width: 200px;
-  height: 200px;
-  background-color: pink;
+.todo-input {
+  flex: 1;
+  height: 60px;
+  border-radius: 10px;
 }
-/* 定义入场初始状态 */
-.ani1-enter-from {
-  transform: translateX(-500px) rotate(360deg) scale(0.1);
+.todo-lists {
+  margin-top: 30px;
+}
+.todo-item {
+  height: 40px;
+  line-height: 40px;
+  border-bottom: 1px solid #333;
+}
+.actions {
+  float: right;
+}
+.todo-done {
+  text-decoration: line-through;
+  background-color: #beb9b9;
+}
+.todo-undone {
+  background-color: #2bd689;
+}
+
+.ani-enter-from {
+  transform: translateX(-100%);
   opacity: 0;
 }
-/* 定义出场最终状态 */
-.ani1-leave-to {
-  transform: translateY(500px) rotate(360deg) scale(0.1);
+.ani-leave-to {
+  transform: translateX(100%);
   opacity: 0;
 }
-.ani1-enter-active,
-.ani1-leave-active {
+.ani-enter-active,
+.ani-leave-active {
   transition: all 1s;
 }
 </style>
