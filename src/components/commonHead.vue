49e2cdea19c8c802f4a7af31f35be11a75eae558
xmj <xmj@163.com>
2023-09-27 19:33:41 +0800

    回到顶部

--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    <input type="text" v-focus="4">
+    <button v-backTop>回到顶部</button>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  directives: {
+    backTop: {
+      mounted(el, { value = 200 }) {
+        el.style.display = "none";
+        window.onscroll = () => {
+          let st =document.documentElement.scrollTop || document.body.scrollTop;
+          if (st > value) {
+            el.style.display = "block";
+          } else {
+            el.style.display = "none";
+          }
+        };
+        el.onclick = () => {
+          window.scrollTo(0, 0);
+        };
+      },
+      unmounted() {
+        window.onscroll = null;
+      },
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+button {
+  position: fixed;
+  right: 50px;
+  bottom: 100px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 let app = createApp(App)
-// 自定义全局指令
-app.directive('focus',{
-    mounted(el,binding){
-       el.focus()
-    }
-})  
+
+// app.directive('backTop',{
+//     mounted(el,{value=200}){
+//        el.style.display = 'none'
+//        window.onscroll = ()=>{
+//         let st = document.documentElement.scrollTop || document.body.scrollTop
+//         if(st>value){
+//             el.style.display = 'block'
+//         }else{
+//             el.style.display = 'none'
+//         }
+//        }
+//        el.onclick = ()=>{
+//         window.scrollTo(0,0)
+//        }
+//     },
+//     unmounted(){
+//         window.onscroll = null
+//         el.onclick = null
+//     }
+// })  
 app.mount('#app')
