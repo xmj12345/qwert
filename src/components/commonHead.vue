dccdcfd7cd31d3789aaa9506c67c93819abe7226
xmj <xmj@163.com>
2023-09-27 21:40:28 +0800

    a

--- src/components/commonHead.vue
+++ src/components/commonHead.vue
@@ -1,39 +1,26 @@
 <template>
   <div>
-    <button v-backTop>回到顶部</button>
+    <video-player
+    src="/11.mp4"
+    poster=""
+    controls
+    :loop="true"
+    :volume="0.6"
+  />
   </div>
+<CommonNav/>
+<input type="text" v-focus>
+{{ a }}
 </template>
-
 <script>
-export default {
-  directives: {
-    backTop: {
-      mounted(el, { value = 200 }) {
-        el.style.display = "none";
-        window.onscroll = () => {
-          let st =document.documentElement.scrollTop || document.body.scrollTop;
-          if (st > value) {
-            el.style.display = "block";
-          } else {
-            el.style.display = "none";
-          }
-        };
-        el.onclick = () => {
-          window.scrollTo(0, 0);
-        };
-      },
-      unmounted() {
-        window.onscroll = null;
-      },
-    },
-  },
-};
+  export default {
+    inject:['a'],
+    mounted(){
+      console.log(this.$aaa)
+    }
+  }
 </script>
 
-<style scoped>
-button {
-  position: fixed;
-  right: 50px;
-  bottom: 100px;
-}
+<style lang="scss" scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueVideoPlayer from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+import loading from './plugins/loading'
 let app = createApp(App)
-
-// app.directive('backTop',{
-//     mounted(el,{value=200}){
-//        el.style.display = 'none'
-//        window.onscroll = ()=>{
-//         let st = document.documentElement.scrollTop || document.body.scrollTop
-//         if(st>value){
-//             el.style.display = 'block'
-//         }else{
-//             el.style.display = 'none'
-//         }
-//        }
-//        el.onclick = ()=>{
-//         window.scrollTo(0,0)
-//        }
-//     },
-//     unmounted(){
-//         window.onscroll = null
-//         el.onclick = null
-//     }
-// })  
+app.use(VueVideoPlayer)
+app.use(loading)
 app.mount('#app')
