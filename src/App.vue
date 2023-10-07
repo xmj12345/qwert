f1bd44039eb8eefa5ea9e46be7fcde15b71607f2
xmj <xmj@163.com>
2023-10-07 19:54:39 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,41 @@
   <button @click="enterHome">首页</button>
   <button @click="enterNew">新闻页</button>
   <button @click="enterAbout">关于我们页</button>
-  <RouterView />
+  <div class="container">
+    <router-view v-slot="{ Component }">
+      <transition name="an1">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <script>
 export default {
   methods: {
-    enterHome(){
-      this.$router.push('/home')
+    enterHome() {
+      this.$router.push("/home");
     },
-    enterNew(){
-      this.$router.push('/new')
+    enterNew() {
+      this.$router.push("/new");
     },
-    enterAbout(){
-      this.$router.replace('/about')
+    enterAbout() {
+      this.$router.replace("/about");
     },
-  }
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.an1-enter-from {
+  transform: translateY(500px);
+  opacity: 0;
+}
+.an1-enter-active {
+  transition: all 1s;
+}
+.container{
+  max-width: 800px;
+  margin: 50px auto;;
+}
+</style>
